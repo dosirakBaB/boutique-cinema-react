b2350a9e7466a10d997846909b77526877db656a
cfgop23 <cfgop23@gmail.com>
2024-09-23 17:42:58 +0900

    modify : footer 겹침 수정(#8)

--- src/layouts/BasicLayout.jsx
+++ src/layouts/BasicLayout.jsx
@@ -2,19 +2,23 @@ import React from "react";
 import MemberHeader from "./../components/layouts/MemberHeader";
 import Footer from "../components/layouts/Footer";
 import { Outlet } from "react-router-dom";
+import MemberSidebar from "./../components/layouts/MemberSidebar";
 
 export default function BasicLayout() {
   return (
-    <div className="relative min-h-screen">
+    <div className="relative min-h-screen pb-[150px]">
       <div className="mx-auto w-layout">
         <MemberHeader />
       </div>
       <div className="my-5 border-b border-gray-400"></div>
-      <main>
-        <div className="mx-auto min-h-full w-layout">
-          <Outlet />
-        </div>
-      </main>
+      <div className="mx-auto flex w-layout">
+        {/* <MemberSidebar /> */}
+        <main>
+          <div>
+            <Outlet />
+          </div>
+        </main>
+      </div>
       <Footer />
     </div>
   );
